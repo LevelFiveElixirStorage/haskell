17a91d810ba088e4e04bca787110ae6ddbc2e484
Caleb Everett <everett.caleb@gmail.com>
2014-07-02 23:50:34 -0400

    Refactor mapBoard and a few other functions

--- conway.hs
+++ conway.hs
@@ -1,6 +1,5 @@
 module Main (main) where
 
-
 type Board = [[Cell]]
 data Cell = Alive | Dead
 
@@ -11,32 +10,43 @@ main = do s <- getContents
 tick :: String -> String
 tick input = showBoard $ update $ readBoard input
 
+-- Update cells in the board
 update :: Board -> Board
-update b = mapCells (updateCell b) b
+update b = mapBoard (updateCell b) b
 
+-- Update a cell based on the rules of conways game of life
+-- 1. Living Cells with less than 2 neighbors die (under population)
+-- 2. Living Cells with more than 3 neighbors die (over population)
 updateCell :: Board -> (Int, Int, Cell) -> Cell
 updateCell b (x, y, Alive)
   | c < 2     = Dead
   | c > 3     = Dead
   | otherwise = Alive
-  where c = length (livingNeighbors b x y)
+  where c = nearbyPopulation b x y
 
+-- 3. Dead Cells with 3 neighbors become alive (reporoduction)
 updateCell b (x, y, Dead)
   | c == 3    = Alive
   | otherwise = Dead
-  where c = length (livingNeighbors b x y)
+  where c = nearbyPopulation b x y
 
-livingNeighbors :: Board -> Int -> Int -> [Cell]
-livingNeighbors b x y = filter isAlive neighbors where
-  isAlive Alive = True
-  isAlive Dead = False
-  neighbors = map (cell b) points
+-- Number of living cells in this moore neighborhood
+nearbyPopulation :: Board -> Int -> Int -> Int
+nearbyPopulation b x y = length (livingNeighbors) where
+    isAlive Alive = True
+    isAlive Dead = False
+    livingNeighbors = filter isAlive $ neighbors b x y
+
+-- Returns the moore neighborhood at x, y (excludes the center)
+neighbors :: Board -> Int -> Int -> [Cell]
+neighbors b x y = map (cell b) points where
   points = [(x',y') | x' <- [x-1..x+1],
                       y' <- [y-1..y+1],
                       x' /= x || y' /= y,
                       x' >= 0, x' < length b,
                       y' >= 0, y' < length b  ]
 
+-- Gets the value of the cell x, y from the board.
 cell :: Board -> (Int, Int) -> Cell
 cell b (x, y) = b !! x !! y
 
@@ -54,9 +64,22 @@ readCell :: Char -> Cell
 readCell 'X' = Alive
 readCell  _  = Dead
 
-mapCells :: ((Int, Int, Cell) -> b) -> [[Cell]] -> [[b]]
-mapCells f b = map (map f) $ map (\ (x, column) -> map (\ (y, cell) -> (x, y, cell)) (zipIndex column)) (zipIndex b)
+-- Applies function f to each cell in the board
+-- f is passed a tuple of (x, y, cell)
+mapBoard :: ((Int, Int, Cell) -> b) -> Board -> [[b]]
+mapBoard f b = map (map f) $ boxZipIndex b
 
+-- Funky way to add x, y cordinates to each cell in the board
+boxZipIndex :: Board -> [[(Int, Int, Cell)]]
+boxZipIndex b =  map xIndexBoard (zipIndex b) where
+  -- Adds x indexes to each cell in the board
+  xIndexBoard (x, column) = map (yIndexRow x) (zipIndex column)
+  -- Adds y indexes to each cell in the row
+  yIndexRow x (y, cell) = (x, y, cell)
+
+
+-- Pairs each element in the list with it's index,
+-- returning a list of tuples
 zipIndex :: [a] -> [(Int, a)]
 zipIndex xs = zip  indexes xs where
   indexes = [x | x <- [0..(length xs)]]
